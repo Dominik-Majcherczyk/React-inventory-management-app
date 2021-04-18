66a18f40bb8c86f762a153155df5f270fde65d26
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-04-18 16:35:41 +0200

    done with context

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -31,6 +31,7 @@ export default function Signup() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
+
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
