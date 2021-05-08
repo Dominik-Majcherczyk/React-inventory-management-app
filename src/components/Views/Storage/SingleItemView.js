98f8cb6066dfde38ec2f9e554d9aa9eb1c08f444
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-05-08 13:21:18 +0200

    ui done

--- src/components/Views/Storage/SingleItemView.js
+++ src/components/Views/Storage/SingleItemView.js
@@ -9,31 +9,54 @@ import {
   ListGroup,
   Table,
 } from "react-bootstrap";
-
+import { Link, useHistory } from "react-router-dom";
 export default function SingleItemView() {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Larry the Bird</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Item name</h2>
+
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Item</th>
+                <th>status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Microlith of Paradise</td>
+                <td className="text-center">
+                  <Button variant="primary">avialable</Button>{" "}
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Microlith of Paradise</td>
+                <td className="text-center">
+                  <Button variant="warning">pożyczoned</Button>{" "}
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Microlith of Paradise</td>
+                <td className="text-center">
+                  <Button variant="danger">rozjebaned</Button>{" "}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+        <div className="w-100 text-center">
+          <Alert variant="light">last changed by: Jan 08.09.2021</Alert>
+        </div>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Link to="/storage">Cancel</Link>
+      </div>
+    </>
   );
 }
