836417fda1056ab8887b72d18992c72e75f06511
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-05-15 21:44:55 +0200

    dashboard

--- src/components/Views/Dashboard.js
+++ src/components/Views/Dashboard.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { Accordion, Card, ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  return <Card></Card>
+  <Card></Card>
 }
