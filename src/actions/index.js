1b01f90efc99d0adffcd6bac8b668ea8618148f9
Dominik-Majcherczyk <64542753+Dominik-Majcherczyk@users.noreply.github.com>
2021-05-20 23:12:34 +0200

    redux init

---
+++ src/actions/index.js
@@ -0,0 +1,35 @@
+// export const fetchCity = (cityData) => {
+//   return {
+//     type: "GET_CITY",
+//     payload: { cities: cityData },
+//   };
+// };
+// export const fetchCityError = (error) => {
+//   return {
+//     type: "GET_CITY_ERROR",
+//     payload: { error: error.error },
+//   };
+// };
+
+// export const fetchCityWeatherInfo = (cityWeatherInfo) => {
+//   return {
+//     type: "GET_CITY_WEATHER",
+//     payload: {
+//       isDayTime: cityWeatherInfo.IsDayTime,
+//       localObservationDateTime: cityWeatherInfo.LocalObservationDateTime,
+//       temperature: cityWeatherInfo.Temperature,
+//       weatherIcon: cityWeatherInfo.WeatherIcon,
+//       weatherText: cityWeatherInfo.WeatherText,
+//     },
+//   };
+// };
+
+// export const setCity = (city) => {
+//   return {
+//     type: "SET_CITY",
+//     payload: {
+//       cityName: city.cityName,
+//       cityKey: city.cityKey,
+//     },
+//   };
+// };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthProvider } from "./context/AuthContext";
+import { Provider as ReduxProvider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   // <React.StrictMode>
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <ReduxProvider store={store}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ReduxProvider>,
   // </React.StrictMode>,
   document.getElementById("root")
 );
